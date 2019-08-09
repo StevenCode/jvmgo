3f843c7c9f0362760eb45771556e731502afe035
StevenCode <codeSteven@163.com>
2019-08-09 17:32:38 +0800

    类实现

--- src/jvmgo/rtda/frame.go
+++ src/jvmgo/rtda/frame.go
@@ -1,19 +1,23 @@
 package rtda
 
+import "jvmgo/rtda/heap"
+
 // stack frame
 type Frame struct {
 	lower        *Frame
 	localVars    LocalVars
 	operandStack *OperandStack
 	thread       *Thread
+	method       *heap.Method
 	nextPC       int //the next instruction after the call
 }
 
-func newFrame(thread *Thread, maxLocals, maxStack uint) *Frame {
+func newFrame(thread *Thread, method *heap.Method) *Frame {
 	return &Frame{
 		thread:       thread,
-		localVars:    newLocalVars(maxLocals),
-		operandStack: newOperandStack(maxStack),
+		method:       method,
+		localVars:    newLocalVars(method.MaxLocals()),
+		operandStack: newOperandStack(method.MaxStack()),
 	}
 }
 
@@ -25,9 +29,11 @@ func (self *Frame) LocalVars() LocalVars {
 func (self *Frame) OperandStack() *OperandStack {
 	return self.operandStack
 }
-
 func (self *Frame) Thread() *Thread {
 	return self.thread
+}
+func (self *Frame) Method() *heap.Method {
+	return self.method
 }
 func (self *Frame) NextPC() int {
 	return self.nextPC
